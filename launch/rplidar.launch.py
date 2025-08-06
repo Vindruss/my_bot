52adf21bcdfd09e8e671013bd605856379c41ceb
Vindruss <honza.kubala@seznam.cz>
2025-08-06 10:55:17 +0200

    edit lidar launch file

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -8,13 +8,10 @@ def generate_launch_description():
 
         Node(
             package='rplidar_ros',
-            executable='rplidar_composition',
+            executable='rplidar_c1_launch',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-                'frame_id': 'laser_frame',
-                'angle_compensate': True,
-                'scan_mode': 'Standard'
+                'frame_id': 'laser_frame'
             }]
         )
     ])
