722bbe7751e68acaabf17de1abf455a6895788fd
Vindruss <honza.kubala@seznam.cz>
2025-09-03 13:07:47 +0200

    use_sim_time = LaunchConfiguration('use_sim_time')

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -12,7 +12,7 @@ from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
