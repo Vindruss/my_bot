cfdb026eb9d0544ee6a60e6397ce3ccd6a71d633
Vindruss <honza.kubala@seznam.cz>
2025-10-09 10:37:15 +0200

    launch file

--- launch/rplidar_c1_launch_all.py
+++ launch/rplidar_c1_launch_all.py
@@ -12,35 +12,50 @@ import time
 
 
 def generate_launch_description():
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_port2 = LaunchConfiguration('serial_port', default='/dev/ttyUSB1')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='460800')
+    frame_id = LaunchConfiguration('frame_id', default='laser_frame')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Standard')
+    topic_name = LaunchConfiguration('topic_name', default='scan1')
+    topic_name2 = LaunchConfiguration('topic_name', default='scan2')
+    flip_x_axis = LaunchConfiguration('flip_x_axis', default='false')
+
+    
+
+    
 
     lidar1 = Node(
         package='rplidar_ros',
             executable='rplidar_node',
             name='rplidar_node',
-            parameters=[{'channel_type':'serial',
-                         'serial_port': '/dev/ttyUSB0',
-                         'serial_baudrate': '460800',
-                         'frame_id': 'laser_frame',
-                         'inverted': 'false',
-                         'angle_compensate': 'true',
-                         'scan_mode': 'Standard',
-                         'topic_name': 'scan1',
-                         'flip_x_axis': 'false'}],
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port,
+                         'serial_baudrate': serial_baudrate,
+                         'frame_id': frame_id,
+                         'inverted': inverted,
+                         'angle_compensate': angle_compensate,
+                         'scan_mode': scan_mode,
+                         'topic_name': topic_name,
+                         'flip_x_axis': flip_x_axis}],
             output='screen')
 
-    lidar2 = Node(
+    lidar1 = Node(
         package='rplidar_ros',
             executable='rplidar_node',
             name='rplidar_node',
-            parameters=[{'channel_type':'serial',
-                         'serial_port': '/dev/ttyUSB1',
-                         'serial_baudrate': '460800',
-                         'frame_id': 'laser_frame',
-                         'inverted': 'false',
-                         'angle_compensate': 'true',
-                         'scan_mode': 'Standard',
-                         'topic_name': 'scan2',
-                         'flip_x_axis': 'false'}],
+            parameters=[{'channel_type':channel_type,
+                            'serial_port': serial_port2,
+                            'serial_baudrate': serial_baudrate,
+                            'frame_id': frame_id,
+                            'inverted': inverted,
+                            'angle_compensate': angle_compensate,
+                            'scan_mode': scan_mode,
+                            'topic_name': topic_name2,
+                            'flip_x_axis': flip_x_axis}],
             output='screen')
     
 
