ec9ef278a8904432f1669834faaa7cf4269e17d6
Vindruss <honza.kubala@seznam.cz>
2025-10-21 11:45:35 +0200

    oprava

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -181,7 +181,7 @@ def generate_launch_description():
         delayed_controller_manager,
         delayed_mecanum_controller_spawner,
         delayed_joint_broad_spawner,
-        slam,
-        nav,
-        tcp_server
+        slam
+        # nav,
+        # tcp_server
     ])
